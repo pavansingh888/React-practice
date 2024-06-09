c44a54ce0a7bbd8caf3d7336191d3561d2aaf63a
Pavan Singh <psinghp888@gmail.com>
2024-06-09 21:21:44 +0530

    Mark as done feature

--- todoReact/src/TodoList.jsx
+++ todoReact/src/TodoList.jsx
@@ -2,11 +2,11 @@ import { useState } from "react"
 import { v4 as uuidv4 } from 'uuid';
 
 export default function TodoList(){
-    let [todos,settodos]=useState([{task : "Sample task",id : uuidv4() }]);
+    let [todos,settodos]=useState([{task : "Sample task",id : uuidv4(),isDone:false }]);
     let [newTodo,setnewTodo]=useState("");
 
     let addNewTask = ()=>{
-        settodos((prevTodo)=>{ return [...prevTodo,{ task: newTodo,id : uuidv4() }] })
+        settodos((prevTodo)=>{ return [...prevTodo,{ task: newTodo,id : uuidv4(),isDone:false }] })
         setnewTodo("") 
     }
     let updateTodoValue = (event)=>{
@@ -20,18 +20,18 @@ export default function TodoList(){
         return prevTodo.map((todo) => {
             return {
                 ...todo,
-                task : todo.task.toUpperCase()
+                isDone : true
             };
         });
        });
     };
-   let upperCaseOne = (id)=>{
+   let markOneDone = (id)=>{
     settodos((prevTodo)=>{
         return prevTodo.map((todo) => {
             if(todo.id==id){
                  return {
                 ...todo,
-                task : todo.task.toUpperCase()
+                isDone : true
             };}else{
                 return todo;
             }
@@ -51,11 +51,11 @@ export default function TodoList(){
                 {
                 todos.map((todo)=>{
                    return <li key={todo.id}>
-                   <span>{todo.task}</span>
+                   <span style={todo.isDone ? {textDecorationLine: 'line-through'} : {}}>{todo.task}</span>
                    &nbsp;&nbsp;&nbsp;
                    <button onClick={() => deleteTodo(todo.id) //Arrow function doesn't execute the function but simply create a copy of that function.
                    }>Delete</button> 
-                   <button onClick={() => upperCaseOne(todo.id) //Arrow function doesn't execute the function but simply create a copy of that function.
+                   <button onClick={() => markOneDone(todo.id) //Arrow function doesn't execute the function but simply create a copy of that function.
                    }>Uppercase One</button> 
                    </li> 
                 })
