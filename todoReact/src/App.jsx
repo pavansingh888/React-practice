d5a10b97e46f9c98e86fa333300f665e96a24dad
Pavan Singh <psinghp888@gmail.com>
2024-06-15 16:41:29 +0530

    passing Functions as props

--- todoReact/src/App.jsx
+++ todoReact/src/App.jsx
@@ -2,15 +2,21 @@ import { LudoBoard } from './LudoBoard'
 import './App.css'
 import TodoList from './TodoList'
 import LotteryGame from './LotteryGame'
+import { sum } from './helper'
 
 function App() {
 
+  let winCondition=(arr)=>{
+    // return sum(arr)===15;
+    // return arr.every((num)=>num === arr[0])
+    return arr[0]===0;
+  }
 
   return (
     <>
       {/* <LudoBoard/> */}
       {/* <TodoList/> */}
-      <LotteryGame n={3} winningSum={15}/>
+      <LotteryGame n={3} winCondition={winCondition}/>
     </>
   )
 }
--- todoReact/src/LotteryGame.jsx
+++ todoReact/src/LotteryGame.jsx
@@ -1,18 +1,19 @@
 import { useState } from "react"
 import { genTicket,sum } from "./helper"
 import Ticket from "./Ticket"
+import Button from "./Buttton"
 
-export default function LotteryGame({n=3,winningSum=15}) {
+export default function LotteryGame({n=3,winCondition}) {
     let [ticket,setTicket] = useState(genTicket(n))
-    let isWinning = sum(ticket) === winningSum;
+    let isWinning = winCondition(ticket)
     function buyTicket(){
         setTicket(genTicket(n))
     }
     return (
     <>
-    <h1>Lottery Game!</h1>
+    <h1>Lottery Game!</h1> 
     <Ticket ticket={ticket}/>
-    <button onClick={buyTicket}>Buy New ticket</button>
+    <Button action={buyTicket}/>
     <h3>{isWinning && "Congratulations, You won!"}</h3>
     </>
     )
