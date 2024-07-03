0ec594b6412aa94ecbd46662a53e55a3cc155fc3
Pavan Singh <psinghp888@gmail.com>
2024-07-03 13:40:22 +0530

    Weather App using Material UI

---
+++ mini-project-react/src/WeatherApp.jsx
@@ -0,0 +1,26 @@
+import SearchBox from "./SearchBox"
+import InfoBox from "./InfoBox"
+import { useState } from "react"
+export default function WeatherApp(){
+    const [weatherInfo,setWeatherInfo] = useState({
+        city: "Delhi",
+            temp: 24.84,
+            tempMin: 25.85,
+            tempMax: 25.05,
+            humidity: 47,
+            feelsLike: 40.07,
+            weather: "haze",
+    })
+
+    let updateInfo = (newInfo) => {
+        setWeatherInfo(newInfo)
+    }
+
+    return (
+        <div style={{textAlign:'center'}}>
+            <h2>Weather App by Pavan</h2>
+            <SearchBox updateInfo={updateInfo}/>
+            <InfoBox info={weatherInfo}/>
+            </div>
+    )
+}
